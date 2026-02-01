79f0dade4c1b0ac1804da41db2a9e8a5ba1a3e38
sibellavia <moneb1x@gmail.com>
2026-02-01 21:31:57 +0100

    Refined --version flag support. Previous commit did not work as expected

--- cmd/dory/main.go
+++ cmd/dory/main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"os"
+	"runtime/debug"
 
 	"github.com/sibellavia/dory/internal/commands"
 )
 
-var version = "dev"
+var version = "0.2.3"
 
 func main() {
-	commands.RootCmd.Version = version
+	commands.RootCmd.Version = getVersion()
 	if err := commands.RootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
+}
+
+func getVersion() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		if info.Main.Version != "" && info.Main.Version != "(devel)" {
+			return info.Main.Version
+		}
+	}
+	return version
 }
